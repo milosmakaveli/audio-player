b12a3327ff907fda436248632e4b4cc716e1f802
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-08 19:23:01 +0200

    imported assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
 
+
+
+const songsList = [
+  { id: 1, url: "/assets/bymyside.mp3", title: "By My Side", thumbnail: "/assets/img/bymyside.jpg", artist: "Marcus P" },
+  { id: 2, url: "/assets/dawnofchange.mp3", title: "Dawn Of Change", thumbnail: "/assets/img/dawnofchange.jpg", artist: "Roman Senyk" },
+  { id: 3, url: "/assets/slowlife.mp3", title: "Slow Life", thumbnail: "/assets/img/slowlife.jpg", artist: "Benjamin Lazzarus" },
+];
+
+
 function App() {
  
   return (
     <>
-      <p>Hell</p>
+    
     </>
   )
 }
